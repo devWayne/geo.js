97251c9821ea986f3542715ac25de657a5e735fc
Wayne <dev.wayne.ix@gmail.com>
2014-10-23 11:53:46 +0800

    concatfuc

--- geo.js
+++ geo.js
@@ -1,9 +1,6 @@
-var geo = function() {
-		this.deg2rad = Math.PI / 180;
-		this.rad2deg = 180 / Math.PI;
-		this.earthRad = 6371;
-		this.earthDia = this.earthRad * 2;
-	}
+	var geo = function() {
+		}
+	
 
 	var proto = geo.prototype;
 
@@ -11,29 +8,12 @@ var geo = function() {
 
 
 	proto.distance = function(geo1, geo2) {
-		var latCenterRad = geo1.latitude,
-			lonCenterRad = geo1.longitude,
-			latVals = geo2.latitude,
-			lonVals = geo2.longitude;
 
-		//计算经纬度
-		var latRad = latVals * this.deg2rad;
-		var lonRad = lonVals * this.deg2rad;
-
-		//计算经纬度的差
-		var diffX = latCenterRad * this.deg2rad - latRad;
-		var diffY = lonCenterRad * this.deg2rad - lonRad;
-		//计算正弦和余弦
-		var hsinX = Math.sin(diffX * 0.5);
-		var hsinY = Math.sin(diffY * 0.5);
-		var latCenterRad_cos = Math.cos(latCenterRad * this.deg2rad);
-		var h = hsinX * hsinX + (latCenterRad_cos * Math.cos(latRad) * hsinY * hsinY);
-
-		var distance=this.earthDia * Math.atan2(Math.sqrt(h), Math.sqrt(1 - h));
+		var distance=proto.value(geo1,geo2);
 
 		if(distance>1){
-			var _km=distance-distance%1000;
-			return _km.toString()+'km';
+			var _km=distance-distance%1;
+			return '>'+_km.toString()+'km';
 		}
 		else{
 			var _m=parseInt(distance*1000);
@@ -43,38 +23,52 @@ var geo = function() {
 	}
 
 	proto.value = function(geo1, geo2) {
+		var deg2rad = Math.PI / 180;
+		var rad2deg = 180 / Math.PI;
+		var earthRad = 6371;
+		var earthDia = earthRad * 2;
+
 		var latCenterRad = geo1.latitude,
 			lonCenterRad = geo1.longitude,
 			latVals = geo2.latitude,
 			lonVals = geo2.longitude;
 
 		//计算经纬度
-		var latRad = latVals * this.deg2rad;
-		var lonRad = lonVals * this.deg2rad;
+		var latRad = latVals * deg2rad;
+		var lonRad = lonVals * deg2rad;
 
 		//计算经纬度的差
-		var diffX = latCenterRad * this.deg2rad - latRad;
-		var diffY = lonCenterRad * this.deg2rad - lonRad;
+		var diffX = latCenterRad * deg2rad - latRad;
+		var diffY = lonCenterRad * deg2rad - lonRad;
 		//计算正弦和余弦
 		var hsinX = Math.sin(diffX * 0.5);
 		var hsinY = Math.sin(diffY * 0.5);
-		var latCenterRad_cos = Math.cos(latCenterRad * this.deg2rad);
+		var latCenterRad_cos = Math.cos(latCenterRad * deg2rad);
 		var h = hsinX * hsinX + (latCenterRad_cos * Math.cos(latRad) * hsinY * hsinY);
 
-		return this.earthDia * Math.atan2(Math.sqrt(h), Math.sqrt(1 - h));
+		return earthDia * Math.atan2(Math.sqrt(h), Math.sqrt(1 - h));
 
 	}
 
-	proto.getLocation = function(callback) {
+	proto.getLocation = function(successCb,errorCb) {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
 				window.latitude=position.coords.latitude;
 				window.longitude=position.coords.longitude;
-				if (callback && (callback instanceof Function)) {
-							callback();
+				if (successCb && (successCb instanceof Function)) {
+							successCb();
+				}
+			},
+			function(){
+				if (errorCb && (errorCb instanceof Function)) {
+							errorCb();
 				}
 			});
 		} else {
 			console.log("Geolocation is not supported by this browser.");
 		}
 	}
+
+
+
+
